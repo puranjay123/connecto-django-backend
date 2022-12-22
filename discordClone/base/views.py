2a8497bdd1aee974c3e3de108f2898168ac07877
puranjay123 <kwattrapuranjay@gmail.com>
2022-12-22 06:11:07 +0530

    added files

--- discordClone/base/views.py
+++ discordClone/base/views.py
@@ -99,6 +99,8 @@ def home(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+
+    participants =room.participants.all()
     if request.method=='POST':
         message = Message.objects.create(
             user = request.user,
@@ -107,7 +109,7 @@ def room(request,pk):
         )
         return redirect('room',pk =room.id)
 
-    context = {'room':room,'room_messages':room_messages}
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
 
     return render(request,'base/room.html',context)
 @login_required(login_url = 'login')
